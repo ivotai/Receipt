e9bb72c73523427e375e413162832d37e90b8d8d
ivotai <ivotai@163.com>
2021-08-16 09:29:04 +0800

    添加 koin 依赖，做了一些基本测试，测试成功。

--- app/src/main/java/com/example/receipt/App.kt
+++ app/src/main/java/com/example/receipt/App.kt
@@ -1,11 +1,31 @@
 package com.example.receipt
 
 import androidx.multidex.MultiDexApplication
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(appModule)
+        }
     }
 
+    private val appModule = module {
+        single<BaseUrl> { BaseUrlImpl() }
+    }
+}
+
+
+interface BaseUrl {
+    val value: String
+}
+
+class BaseUrlImpl : BaseUrl {
+    override val value = "www.baidu.com"
 }
--- app/src/main/java/com/example/receipt/MainActivity.kt
+++ app/src/main/java/com/example/receipt/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.receipt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    val baseUrl by inject<BaseUrl>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        baseUrl
+        baseUrl
     }
 
 }

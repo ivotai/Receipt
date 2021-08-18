d43d342907fff67b6a8719a76a72d6c1b0c50c4e
ivotai <ivotai@163.com>
2021-08-18 17:58:07 +0800

    使用 JExcelApi 生成了简单的 Excel。 poi 库的依赖有问题，android 用不了。

--- app/src/main/java/com/example/receipt/MainActivity.kt
+++ app/src/main/java/com/example/receipt/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.receipt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jxl.LabelCell
+import jxl.Workbook
+import jxl.write.Label
 import org.koin.android.ext.android.inject
 import org.koin.core.qualifier.named
 import retrofit2.Retrofit
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +20,18 @@ class MainActivity : AppCompatActivity() {
 
         retrofit
         retrofit
+
+        s()
     }
 
+
+    private fun s(){
+        val file = File(cacheDir,"2.xls")
+        val workbook = Workbook.createWorkbook(file)
+        val sheet = workbook.createSheet("测试",0)
+        val label = Label(0,0,"hehe")
+        sheet.addCell(label)
+        workbook.write()
+        workbook.close()
+    }
 }
